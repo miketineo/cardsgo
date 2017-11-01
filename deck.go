26969e2166d7e194669b438aadd24e4eb154339e
Miguel Tineo <miketineo@gmail.com>
2017-11-01 23:15:21 +0000

    good stuff

--- deck.go
+++ deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -33,10 +36,30 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0777)
 }
 
-func (d deck) toString() string {
-	return strings.Join([]string(d), ",")
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		// Swap
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 func main() {
+	// cards := newDeck()
+	// cards.saveToFile("mycards")
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+	// deck := newDeckFromFile("mycards")
+	// deck.print()
 	cards := newDeck()
-	cards.saveToFile("mycards")
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
+	cards.shuffle()
+	cards.print()
 }
